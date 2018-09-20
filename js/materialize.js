4ced09fb891af39935879b3344a4cce22111d265
spacedgatito <miluska.gonzalezar@gmail.com>
2018-09-20 14:34:41 -0500

    Actualiza maquetación

---
+++ js/materialize.js
@@ -0,0 +1,13 @@
+const { ScrollSpy, Dropdown } = M;
+
+Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {
+  aligment: 'right', coverTrigger: false, constrainWidth: false,
+});
+
+const scrollSpyElements = document.querySelectorAll('.scrollspy');
+
+ScrollSpy.init(scrollSpyElements, { scrollOffset: (window.innerWidth < '601' ? 57 : 65) });
+
+window.addEventListener('resize', () => {
+  ScrollSpy.init(scrollSpyElements, { scrollOffset: (window.innerWidth < '601' ? 57 : 65) })
+});
